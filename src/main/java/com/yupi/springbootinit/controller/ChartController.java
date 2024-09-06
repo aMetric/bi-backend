9dac50a21a45a0a4fb50dd5040b35a42edf07719
coderwhs <coderwhs@gmail.com>
2024-09-06 21:53:21 +0800

    线程池初尝试

--- src/main/java/com/yupi/springbootinit/controller/ChartController.java
+++ src/main/java/com/yupi/springbootinit/controller/ChartController.java
@@ -283,7 +283,7 @@ public class ChartController {
         ThrowUtils.throwIf(size > ONE_MB, ErrorCode.PARAMS_ERROR, "文件超过 1M");
         // 校验文件后缀 aaa.png
         String suffix = FileUtil.getSuffix(originalFilename);
-        final List<String> validFileSuffixList = Arrays.asList("xlsx");
+        final List<String> validFileSuffixList = Arrays.asList("xlsx","xls");
         ThrowUtils.throwIf(!validFileSuffixList.contains(suffix), ErrorCode.PARAMS_ERROR, "文件后缀非法");
 
         User loginUser = userService.getLoginUser(request);
