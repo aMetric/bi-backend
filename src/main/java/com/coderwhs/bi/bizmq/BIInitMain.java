77c9448b716b8c65a6ab2b519cb4fc0130f99419
coderwhs <coderwhs@gmail.com>
2024-10-20 23:50:26 +0800

    ssl证书配置

--- src/main/java/com/coderwhs/bi/bizmq/BIInitMain.java
+++ src/main/java/com/coderwhs/bi/bizmq/BIInitMain.java
@@ -12,36 +12,38 @@ import javax.annotation.PostConstruct;
 /**
  * 程序用到的交换机和队列（只用在程序启动前执行一次）
  */
-// @Component
+@Component
 @Slf4j
 public class BIInitMain {
 
-    // @PostConstruct
-    // public void mqInit() throws Exception {
-    //     try {
-    //         log.debug("------------------【BIInitMain】开始队列初始化");
-    //         // 创建连接工厂
-    //         ConnectionFactory factory = new ConnectionFactory();
-    //         factory.setHost("localhost");
-    //         // 创建连接
-    //         Connection connection = factory.newConnection();
-    //         // 创建通道
-    //         Channel channel = connection.createChannel();
-    //         // 定义交换机的名称为"code_exchange"
-    //         String EXCHANGE_NAME = BiMqConstant.BI_EXCHANGE_NAME;
-    //         // 声明交换机，指定交换机类型为 direct
-    //         channel.exchangeDeclare(EXCHANGE_NAME, "direct");
-    //
-    //         // 创建队列，随机分配一个队列名称
-    //         String queueName = BiMqConstant.BI_QUEUE_NAME;
-    //         // 声明队列，设置队列持久化、非独占、非自动删除，并传入额外的参数为 null
-    //         channel.queueDeclare(queueName, true, false, false, null);
-    //         // 将队列绑定到交换机，指定路由键为 "my_routingKey"
-    //         channel.queueBind(queueName, EXCHANGE_NAME, BiMqConstant.BI_ROUTING_KEY);
-    //     } catch (Exception e) {
-    //         throw new Exception(e);
-    //     }
-    // }
+    @PostConstruct
+    public void mqInit() throws Exception {
+        try {
+            log.debug("------------------【BIInitMain】开始队列初始化");
+            // 创建连接工厂
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setUsername("coderwhs");
+            factory.setPassword("@waiseng1204");
+            factory.setHost("117.72.86.124");
+            // 创建连接
+            Connection connection = factory.newConnection();
+            // 创建通道
+            Channel channel = connection.createChannel();
+            // 定义交换机的名称为"code_exchange"
+            String EXCHANGE_NAME = BiMqConstant.BI_EXCHANGE_NAME;
+            // 声明交换机，指定交换机类型为 direct
+            channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+
+            // 创建队列，随机分配一个队列名称
+            String queueName = BiMqConstant.BI_QUEUE_NAME;
+            // 声明队列，设置队列持久化、非独占、非自动删除，并传入额外的参数为 null
+            channel.queueDeclare(queueName, true, false, false, null);
+            // 将队列绑定到交换机，指定路由键为 "my_routingKey"
+            channel.queueBind(queueName, EXCHANGE_NAME, BiMqConstant.BI_ROUTING_KEY);
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         try {
