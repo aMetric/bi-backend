66b8ce49e29981c33c472c1a174d906dd995c480
coderwhs <coderwhs@gmail.com>
2024-11-24 18:33:02 +0800

    aigc优化

--- src/main/java/com/coderwhs/bi/manager/AIManager.java
+++ src/main/java/com/coderwhs/bi/manager/AIManager.java
@@ -72,11 +72,8 @@ public class AIManager {
     public String sendMsgToXingHuo(boolean isNeedTemplate, String content) {
         if (isNeedTemplate) {
             // AI 生成问题的预设条件
-            String predefinedInformation = "你是一个数据分析师和前端开发专家，接下来我会按照以下固定格式给你提供内容：\n" +
-                    "分析需求：\n" +
-                    "{数据分析的需求或者目标}\n" +
-                    "原始数据：\n" +
-                    "{csv格式的原始数据，用,作为分隔符}\n" +
+          content = "你是一个数据分析师和前端开发专家，接下来我会按照以下固定格式给你提供内容：\n" +
+                    content +
                     "请根据这两部分内容，严格按照以下指定格式生成内容（此外不要输出任何多余的开头、结尾、注释，如果最后生成的内容中包含单引号，一定要将单引号替换成空格）\n" +
                     "'#########'\n" +
                     "{前端 Echarts V5 的 option 配置对象json代码，json代码的键值对都一定要加上英文双引号，跟下面的示例一样。合理地将数据进行可视化，不要生成任何多余的内容，比如注释}\n" +
@@ -87,8 +84,8 @@ public class AIManager {
                     + jsonData
                     + "#########\n" +
                     "结论："
-                    + resTemplate;
-            content = predefinedInformation + "\n" + content;
+                    + resTemplate
+                    +"请按照模板的格式严格返回";
         }
         List<SparkMessage> messages = new ArrayList<>();
         messages.add(SparkMessage.userContent(content));
@@ -100,9 +97,8 @@ public class AIManager {
                 .maxTokens(2048)
                 // 核采样阈值。用于决定结果随机性,取值越高随机性越强即相同的问题得到的不同答案的可能性越高 非必传,取值为[0,1],默认为0.5
                 .temperature(0.2)
-                // 指定请求版本，默认使用最新2.0版本
+                // 指定请求版本
                 .apiVersion(SparkApiVersion.V4_0)
-                // .apiVersion(SparkApiVersion.V3_5)
                 .build();
         // 同步调用
         SparkSyncChatResponse chatResponse = sparkClient.chatSync(sparkRequest);
